d7f9a31ccdcc0bb6decd23773d9e04134c47c727
Masami Sugao <15377854+masami-sugao@users.noreply.github.com>
2025-08-02 16:02:16 +0900

    :globe_with_meridians: ローカライズ

--- MidiSevenBitsAndBytesConverter/MidiSevenBitsAndBytesConverter/Program.cs
+++ MidiSevenBitsAndBytesConverter/MidiSevenBitsAndBytesConverter/Program.cs
@@ -10,7 +10,9 @@ namespace LilytechLab.MidiSevenBitsAndBytesConverter {
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-           	builder.Services.AddMudServices();
+            builder.Services.AddMudServices();
+
+            builder.Services.AddLocalization();
 
             await builder.Build().RunAsync();
         }
